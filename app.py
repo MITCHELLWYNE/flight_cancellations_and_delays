be8471e1f45f46a5676dc20b5ec5f81a57d088dd
Anja Huhmer <ahuehmer@gmail.com>
2023-05-18 19:46:42 -0700

    app.py starter

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+###########################
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to each table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+###########################
+# Flask Setup
+app = Flask(__name__)
+
+###########################
+# Flask Routes
