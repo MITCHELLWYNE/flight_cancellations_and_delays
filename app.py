96e76d0150ab1f5fa040a5d4dd27db1c4f98b022
Anja Huhmer <ahuehmer@gmail.com>
2023-05-21 23:19:33 -0700

    Graph attempt

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -7,24 +6,44 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 ###########################
 # Database Setup
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///flight_cancellations.sqlite")
 
 # reflect an existing database into a new model
-Base = automap_base()
+# Base = automap_base()
 
 # reflect the tables
-Base.prepare(autoload_with=engine)
+# Base.prepare(autoload_with=engine)
 
 # Save references to each table
-measurement = Base.classes.measurement
-station = Base.classes.station
+# measurement = Base.classes.measurement
+
+# Create session
+session = Session(engine)
 
 ###########################
 # Flask Setup
 app = Flask(__name__)
+CORS(app)
 
 ###########################
 # Flask Routes
+
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"api/airline_delay_cause.json<br/>"
+    )
+
+@app.route("/api/airline_delay_cause.json")
+def airline_delay_cause():
+    results = engine.execute("SELECT * FROM airline_delay_cause")
+    return jsonify ([dict(_) for _ in results])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,139 @@
+// Create URL variable
+// const url = "http://127.0.0.1:5000/api/airline_delay_cause.json";
+// const url = "{{url_for('airline_delay_cause')}}"
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
+
+// =================================================================================
+
+// THIS ONLY RUNS IF I PYTHON3 APP.PY RUN APP.PY IN THE TERMINAL AND HAVE THE JSON PAGE OPEN
+
+// =================================================================================
+
+// Read in JSON 
+d3.json(url).then(function(dataPromise) {
+    console.log(dataPromise);
+    console.log(dataPromise[1].airport);
+
+    for (let i = 0; i < dataPromise.length; i++) {
+        let months = dataPromise[i].month;
+        console.log(months);
+        //let lateArrival = dataPromise[i].arr_delayed;
+        //console.log(lateArrival);
+        //let filterLateArrival = lateArrival.filter(sample => dataPromise[i].airport === airport);
+        //let carrierCancellation = dataPromise[i].carrier_ct; // are these carrier cancellations?
+        //let filterarrierCancellation = carrierCancellation.filter(sample => dataPromise[i].airport === airport);
+    };
+
+});
+
+// Plots
+function Plots(airport) {
+
+    d3.json(url).then(function(dataPromise) {
+        
+        // Create data references  
+        for (let i = 0; i < dataPromise.length; i++) {
+            let months = dataPromise[i].month;
+            console.log(months);
+            let lateArrival = dataPromise[i].arr_delayed;
+            console.log(lateArrival);
+            let filterLateArrival = lateArrival.filter(dataPromise => dataPromise[i].airport === airport);
+            let carrierCancellation = dataPromise[i].carrier_ct; // are these carrier cancellations?
+            let filterarrierCancellation = carrierCancellation.filter(sample => dataPromise[i].airport === airport);
+        };
+
+        // Line Chart
+        let cancellations = {
+            x: filterLateArrival,
+            y: months,
+            type: 'scatter'
+          };
+          
+          let delays = {
+            x: filterarrierCancellation,
+            y: months,
+            type: 'scatter'
+          };
+          
+          let lineData = [cancellations, delays];
+          
+          Plotly.newPlot("line", lineData);
+
+        // Bar Chart 
+        // let graphData = {
+            // x: sampleValueTop10,
+            //y: otuIdTop10,
+            //text: otuLabelTop10,
+            //type: "bar",
+            //orientation: "h"
+        //};
+  
+        // Data trace array
+        //let traceData = [graphData];
+
+        //let layout = {
+            //height: 600,
+            //: 800
+        //};
+    
+        //Plotly.newPlot("bar", traceData, layout);
+
+    });
+
+}
+
+// Init function for dropdown menu 
+function init() {
+    d3.json(url).then(function(dataPromise) {
+
+        // Select dropdown menu and grab information
+        let dropdownMenu = d3.select("#selDataset");
+        for (let i = 0; i < dataPromise.length; i++) {
+            let airport_name = dataPromise.airport_name[i];
+        };
+
+        // Append IDs to dropdown menu
+        airport_name.forEach((airport) => {
+            dropdownMenu.append("option").text(airport)
+        });
+    
+        // Determine inital graph data 
+        let firstData = airport_name[0];
+
+        // Display inital graphs 
+        Plots(firstData);
+        //Demographics(firstData);
+    });
+};
+
+// Demographics 
+//function Demographics(id) {
+
+    // Grab information
+    //d3.json(url).then(function(data) {
+
+        // Filter metadata for each ID
+        //let demoInfo = data.metadata.filter(info => info.id == id);
+
+        // Grab first value
+        //let firstDemo = demoInfo[0];
+
+        // Select metadata location in html file
+        //d3.select("#sample-metadata").html("");
+
+        // Append each key-value pair 
+        //Object.entries(firstDemo).forEach(([key, value]) => {
+            //console.log(key, value);
+            //d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        //});
+    //});
+//};
+
+// Update the Plots
+function optionChanged(value) { 
+    Plots(value);
+    //Demographics(value);
+}
+  
+init();
