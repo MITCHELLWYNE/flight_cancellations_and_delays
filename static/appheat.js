9adc1cc69935234b49768cb1b0f78c4d2f8bd2ca
MichaelMitch <mitchellwyne@gmail.com>
2023-05-25 18:35:18 -0700

    starter for heatmap

--- static/appheat.js
+++ static/appheat.js
@@ -1,7 +1,21 @@
-import pandas as pd
-import random
-import numpy as np
-import matplotlib.lyplot as plt
+d3.csv('Airline_Delay_Cause 2022.csv').then(function(data) {
+    var parseData = data.map(function(d) {
+        return {
+            x: +d.arr_flights, 
+            y: +d.month,
+            value: +d.arr_del15            
+        };
+    });
 
 
-var data = []
+var heatmap = d3.select("heatmap_element")
+.selectAll("div")
+.data(parsedData)
+.enter()
+.append("div")
+.style("width", "20px")
+.style("height", "20px")
+.style("background-color", function(d){
+    return d3.interpolateBlues(d.values);
+    }) 
+});
